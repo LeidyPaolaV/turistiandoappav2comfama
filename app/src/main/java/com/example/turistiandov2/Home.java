440d271421208de84d4b1b0df9231fc96fc2cc3c
Jander Buritica <janderburitica123@gmail.com>
2023-06-27 20:42:59 -0500

    se crea proyecto turistiandov2

---
+++ app/src/main/java/com/example/turistiandov2/Home.java
@@ -0,0 +1,36 @@
+package com.example.turistiandov2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Home extends AppCompatActivity {
+
+//zona de atributos
+    Button botonHoteles;
+    Button botonRestaurantes;
+    Button botonTurismo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        botonHoteles=findViewById(R.id.botonhoteles);
+        botonRestaurantes=findViewById(R.id.botonrestaurantes);
+        botonTurismo=findViewById(R.id.botonsitios);
+
+
+        //detectando eventos
+
+        botonHoteles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "hice click", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
